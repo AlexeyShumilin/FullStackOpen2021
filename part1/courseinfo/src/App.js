b468ad037c0d4f943ea3c0637f998e3c46bc36aa
Alexey Shumilin <shumil.alexey@gmail.com>
2021-12-18 14:39:27 +0200

    Exercises 1.3.-1.5.
    add Exercises 1.3.-1.5.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,14 +5,21 @@ import {Content} from "./Content";
 
 const App = () => {
     const course = {
-
-        name: "Half Stack application development", parts: [{
-            name: "Fundamentals of React", exercises: 10
-        }, {
-            name: "Using props to pass data", exercises: 7
-        }, {
-            name: "State of a component", exercises: 14
-        }]
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
     }
 
     return (
